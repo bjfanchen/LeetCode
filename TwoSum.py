15e17fed1bd5bc32fed6a21f5e4513e7f4154eab
bjfanchen <pursuefc@163.com>
2018-04-20 15:05:18 +0800

    add some messages to first two files

--- TwoSum.py
+++ TwoSum.py
@@ -3,9 +3,18 @@
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+一句话描述题目：
+两个数之和已知target，从list中找出这两个数
+
+解题思路：
+1.两次遍历，每两个数都相加一次，再比较. 可行，但时间复杂度太高，约为O(n^2)
+2.利用HashTable来实现，一次遍历list，迭代list元素将其添加进HashTable中，
+  同时查找（通过in关键字来实现）table中已有元素与当前元素之和是否为target，查找成功即返回
 """
 
 
+# 代码实现
 class Solution:
     def twoSum(self, nums, target):
         """
@@ -16,7 +25,7 @@ class Solution:
         d = {}
         for i, n in enumerate(nums):
             m = target - n
-            if m in d:
+            if m in d:  # 使用in查询指定键值是否存在于dict中
                 return [d[m], i]
             else:
                 d[n] = i
--- rmDup.py
+++ rmDup.py
@@ -3,7 +3,24 @@
 Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
 
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+一句话描述题目：
+有序list去重
+
+解题思路：
+Since the array is already sorted, we can keep two pointers i and j, where i is the slow-runner while j is the fast-runner.
+As long as nums[i] = nums[j], we increment j to skip the duplicate.
+
+When we encounter nums[j]≠nums[i], the duplicate run has ended so we must copy its value to nums[i+1].
+i is then incremented and we repeat the same process again until j reaches the end of array.
+
+设定两个指针i和newTail，i用于iterate对每个元素进行检查，newTail用于标记去重过程中list的尾部( i > newTail )
+当nums[i]和nums[newTail]相等时，不做任何操作，i递增
+当nums[i]和nums[newTail]不等时，newTail递增，并将nums[i]赋值给nums[newTail]
+当i执行到list最后一个元素，循环结束，newTail+1即是去重list元素个数
+注意：1.去重后list大小不变，区别在于前（newTail+1）个元素是不重复的，后面的元素与原list相同
+     2.remove实现不可行，原因在于remove掉重复元素后，list变短，原来指针指向的元素变化，可能会跳过某些元素的去重比较
 """
+
 
 class Solution:
     def removeDuplicates(self, nums):
@@ -27,3 +44,9 @@ class Solution:
                 nums[newTail] = nums[i]
 
         return newTail + 1
+
+
+"""
+时间复杂度：O(n)
+空间复杂度：O(1)
+"""
